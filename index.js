f50c2daf7289495a44656a1d350228057bcb6bc8
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-07-13 20:32:43 +0700

    [Update]Menambahkan App Use untuk mengecek request

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();  //object of express
 
 
+
+app.use(()=> {
+    console.log('We got a new request');
+});
+
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
 });
