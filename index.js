72ca86af8bb63f5989be480354c409c1493e255e
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-07-13 21:26:49 +0700

    [Add]Berhasil menambahkan Nodemon

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();  //object of express
 
+// app.use((req, res)=> {
+//     console.log('We got a new request');
+//     res.send('<h1>This is my webpage</h1>');
+// });
 
+//ROUTE 
+app.get('/cats', (req, res) => {
+    console.log('New request to /cats');
+    res.send('Meow');
+});
 
-app.use((req, res)=> {
-    console.log('We got a new request');
-    res.send('<h1>This is my webpage</h1>');
+app.get('/dogs', (req, res) => {
+    console.log('New request to /dogs');
+    res.send('Hoof');
 });
 
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
 });
+
+
 
