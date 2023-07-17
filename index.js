0b346bb61547a5f849c47152407178ece84ba747
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-07-17 16:06:12 +0700

    [Add]Berhasil menambahkan metode Post data & Interfacenya

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const app = express();  //object of express
 
 app.set('view engine', 'ejs');
 
+// Menyajikan file statis dari direktori "views"
+app.use(express.static('views'));
+
+
+
+//Route Aplikasi
 app.get('/home', (req, res) => {
     res.render('home');    
 });
 
+
+//Startup Server 
 app.listen(8080, () => {
     console.log('Server is running on http://localhost:8080');
 });
